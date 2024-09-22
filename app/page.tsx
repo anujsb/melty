272ea9e588c6aaad190e8969c49c5d69c667fb4d
anuj <anujsbhuyar@gmail.com>
2024-09-22 23:21:14 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { BackgroundCellAnimation } from "@/components/Hero";
+import { FloatingNavDemo } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <FloatingNavDemo />
       <BackgroundCellAnimation />
     </div>
   );
--- components/Video.tsx
+++ components/Video.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Video = () => {
   return (
-    <div id="video">
+    <div id="video" className="flex items-center justify-center">
       {/* <div className="md:w-[1130px] lg:w-[1130px] sm:w-[375px] md:h-[600px] lg:h-[600px] sm:h-[600px] rounded-xl mt-10 z-50">
         <iframe
         //   src="https://www.loom.com/embed/b9ec564cd4d04a6d985c077088794fe6?sid=b6dbae93-f413-4b83-b684-c353fd11ace4"
